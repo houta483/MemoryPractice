2de00ec6af537b38812321b24afbef2cfe667dd3
Tanner Houghton <houta483@uchicago.edu>
2020-06-01 13:29:35 -0500

    added front-end

---
+++ memory_practice_front_end/src/components/HelloWorld.vue
@@ -0,0 +1,109 @@
+<template>
+  <div>
+    <div class="grid-container">
+      <div class="header">
+      <h2>Memory Application</h2>
+    </div>
+  
+    <!-- need to add routes for this -->
+    <div class="left" style="background-color:#aaa;">Recent Tries</div>
+
+    <div class="middle" style="background-color:#bbb;">
+      Test
+      <ShowDigits />
+    </div>  
+
+    
+    <!-- need to add routes for this -->
+    <div class="right" style="background-color:#ccc;">Record</div>
+  
+    <div class="footer"/>
+    <p>Footer</p>
+  </div>
+</div>
+
+</template>
+
+<script>
+import ShowDigits from './ShowDigits';
+
+export default {
+  name: 'HelloWorld',
+  components: { ShowDigits },
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+/* Style the header */
+.header {
+  grid-area: header;
+  background-color: #f1f1f1;
+  padding: 30px;
+  text-align: center;
+  font-size: 35px;
+}
+
+/* The grid container */
+.grid-container {
+  display: grid;
+  grid-template-areas: 
+    'header header header header header header' 
+    'left middle middle middle middle right' 
+    'footer footer footer footer footer footer';
+  /* grid-column-gap: 10px; - if you want gap between the columns */
+} 
+
+.left,
+.middle,
+.right {
+  padding: 10px;
+  height: 300px; /* Should be removed. Only for demonstration */
+}
+
+/* Style the left column */
+.left {
+  grid-area: left;
+}
+
+/* Style the middle column */
+.middle {
+  grid-area: middle;
+}
+
+/* Style the right column */
+.right {
+  grid-area: right;
+}
+
+/* Style the footer */
+.footer {
+  grid-area: footer;
+  background-color: #f1f1f1;
+  padding: 10px;
+  text-align: center;
+}
+
+/* Responsive layout - makes the three columns stack on top of each other instead of next to each other */
+@media (max-width: 600px) {
+  .grid-container  {
+    grid-template-areas: 
+      'header header header header header header' 
+      'left left left left left left' 
+      'middle middle middle middle middle middle' 
+      'right right right right right right' 
+      'footer footer footer footer footer footer';
+  }
+}
+</style>
---
+++ memory_practice_front_end/src/components/ShowDigits.vue
@@ -0,0 +1,173 @@
+<template>
+  <div>
+    <div class="showInts">
+      <p v-if='game === true && timeout === false'>
+        {{digits}}
+      </p>
+
+      <p v-if='game === true && timeout === true'>
+        {{""}}
+      </p>
+
+      <p v-if='game === false'>
+        Game Over
+      </p>
+    </div>
+
+    <div>
+      <input v-model='inputDigits' class="input" />
+
+      <br>
+      <button @click='click' class='btn'>
+        <p v-if="this.start">
+          Start
+        </p>
+
+        <p v-if="this.continue">
+          Continue
+        </p>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      start: true,
+      continue: false,
+      digits: "",
+      round: 0,
+      inputDigits: "",
+      incorrectInputPairs: "",
+      game: true,
+      timeout: false
+    }
+  },
+  methods: {
+    score () {
+      if (this.round === 0) {
+        return 0
+      }
+      else {
+        return (this.round * 2 - 2)
+      }
+    },
+    async click () {
+      if (this.round === 0) {
+        await this.incrementNums(this.round)
+        this.game = true
+        this.start = false;
+        this.continue = true;
+        await this.timer()
+      }
+      
+      else if (this.inputDigits === this.digits) {
+        await this.timer()
+        await this.incrementNums(this.round);
+        this.timeout = false;
+      }
+
+      else if (this.inputDigits !== this.digits) {
+        alert(` Game Over! \n Your Answer: ${this.inputDigits} \n Correct Answer: ${this.digits}`)
+        await this.incorrectDigitsFunc()
+        await this.submitScoreFunc()
+        await this.reset()
+      }
+    },
+
+    incrementNums (round) {
+      this.digits = '';
+
+      for (let i = 0; i <= round; i++) {
+        let firstNumber = Math.floor(Math.random() * 10)
+        let secondNumber = Math.floor(Math.random() * 10)
+        let combinedNums = String(String(firstNumber) + String(secondNumber));
+        this.digits += combinedNums;
+      }
+      this.round += 1;
+      this.inputDigits = '';
+    },
+
+    incorrectDigitsFunc() {
+      for (let i = 0; i < (Math.floor(this.digits.length)); i+=2) {
+        let digitPair = this.digits.slice(i, i+2);
+        let inputPair = this.inputDigits.slice(i, i+2);
+        
+        if (digitPair !== inputPair) {
+          this.incorrectInputPairs += String(inputPair + '-')
+        }
+      }
+      this.incorrectInputPairs = this.incorrectInputPairs.slice(0, -1)
+    },
+    timer() {
+      let numberOfMiliseconds = this.round * 2000
+      setTimeout(() => { 
+        this.game = true 
+        this.timeout = true 
+      }, numberOfMiliseconds);
+    },
+    reset () {
+      this.start = true
+      this.continue = false
+      this.digits = ""
+      this.round = 0
+      this.inputDigits = ""
+      this.incorrectInputPairs = ""
+      this.game = true
+      this.timeout = false
+    },
+
+    async submitScoreFunc () {
+      fetch('http://localhost:3621/', {
+        method: "POST",
+        mode: 'cors',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          numberOfDigits: this.digits.length !== null ? this.digits.length : 'Not Submitted',
+          incorrectDigits: this.inputDigits.length !== this.digits.length ? 'Inputted incorrect number of digits' : this.incorrectInputPairs,
+          mistakenStringOfNums: this.digits === "" ? 'Not Submitted' : this.digits,
+        })
+      })
+    }
+  }
+}
+</script>
+
+<style>
+.showInts {
+  border:black solid 1px;
+  color: gray;
+  -webkit-border-radius: 12px; 
+  margin-top: 12px;
+  height: 50px;
+  background-color: white;
+  text-align: center;
+  padding-top: 11px;
+  font-size: 20px;
+  font-weight: bold;
+}
+.showInts p {
+  margin-top: 6px;
+}
+.input {
+  margin-top: 4%;
+  -webkit-border-radius: 12px; 
+  height: 50px;
+  width: 50%;
+  border:black solid 1px;
+  font-size: large;
+  text-align: center;
+  color: gray
+}
+.btn {
+  margin-top: 3%;
+  -webkit-border-radius: 12px;
+  width: 25%;
+  background-color: #4CAF50; /* Green */
+  height: 50px;
+  font-size: 16px;
+  font-weight: bold;
+}
+</style>
